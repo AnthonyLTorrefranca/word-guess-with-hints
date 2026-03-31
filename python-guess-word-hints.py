e8176d97bdbf63256e12c23434f28c4038e42253
AnthonyLTorrefranca <144714043+AnthonyLTorrefranca@users.noreply.github.com>
2026-03-31 13:02:21 +0800

    feat: implement word-guessing logic using dictionary-based hints
    This implementation uses a dictionary to store hint-word pairs, allowing for easy data retrieval through item unpacking. The core game loop manages the player's six lives and validates that inputs are single alphabetic characters while using a set to keep track of previously guessed letters.

---
+++ python-guess-word-hints.py
@@ -0,0 +1,46 @@
+import random
+moves = {
+    "Holy church has this": "CROSS",
+    "Something chipmunks eat":"NUT",
+    "Even a pirate has this": "HOOK",
+    "Best move to make by a boxer": "UPPERCUT",
+}
+lives = 6
+hint, word = random.choice(list(moves.items()))
+blank = ["_"] * len(word)
+clean = "_".join(blank)
+guessed = set()
+
+while lives > 0 and '_' in blank:
+    print(f'Current life: {lives}\n'
+          f'The hint for the word {hint}\n'
+          f'Word contains {" ".join(blank)}\n'
+    )
+    if lives == 1:
+        print('You have 1 life remaining!')
+
+    guess = input('Input your guess letter one only: ').strip().upper()
+    print()
+
+    found_match = False
+    if len(guess) != 1 or not guess.isalpha():
+        print(f'One character only!\n')
+        continue
+
+
+    for i in range(len(word)):
+        if guess == word[i]:
+            found_match = True
+            blank[i] = guess
+    if not found_match:
+        lives -= 1
+
+    if guess in guessed:
+        print(f"That's guessed already\n")
+
+    guessed.add(guess)
+
+if not '_' in blank:
+    print('Congratulations!')
+else:
+    print('You lost :<')
